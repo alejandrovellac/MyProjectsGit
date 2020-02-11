3f070e63cf9e0596cea13dcc7f12fc9917c69c6d
alejandro <alejandrovellac@monlau.com>
2020-02-11 11:39:54 +0100

    Version 0.0.1

---
+++ git/src/velazquezgit/Velazquezgit.java
@@ -0,0 +1,34 @@
+//author Velazquez
+//4-02-2020
+package velazquezgit;
+
+import java.util.Scanner;
+
+public class Velazquezgit {
+
+    static Scanner keyboard = new Scanner(System.in);
+    
+     public static void main(String[] args) {
+         int dni;
+         char letter;
+                 
+         System.out.print("Write your DNI: ");
+         dni = keyboard.nextInt();
+         letter= DNIletter(dni);
+         System.out.println(dni+":"+letter);
+         
+         String letters ="TRWAGMYFPDXBNJZSQVHLCKE";
+         INT RES=dni%23;
+         letter = letters [];
+         return letter;
+         
+     }
+    
+     private static char DNIletter(int dni){
+        char letter='Z';
+        
+        
+        
+     return letter;
+}
+}
